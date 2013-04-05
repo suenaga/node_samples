5907c3145b4fb94d7e68d80b4b6b016fad5fe65b
Daijiro SUENAGA <info@awks.jp>
2013-04-05 15:46:26 +0900

    Add glob sample

---
+++ my_glob/index.js
@@ -0,0 +1,12 @@
+var glob = require("glob")
+
+options = null;
+options = {statCache: true};
+// options is optional
+glob("**/*.js", options, function (er, files) {
+	// files is an array of filenames.
+	// If the `nonull` option is set, and nothing
+	// was found, then files is ["**/*.js"]
+	// er is an error object or null.
+	console.log(files);
+})
